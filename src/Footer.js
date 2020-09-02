52e7387e69b710db9456ae2ee7f52ec78f6ebaf3
Rheeda-boo <rheeda.beecha@gmail.com>
2020-09-02 06:53:45 +0100

    A tic-tac-toe game

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <centre>
+                <footer>
+                    &copy Rheeda Boo &amp Donny Doe. 2020
+                </footer>
+            </centre>
+        </div>
+    )
+}
